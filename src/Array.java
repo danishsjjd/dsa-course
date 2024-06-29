64df8b91092d56241e2350ad01f5893c275760d2
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-06-29 14:16:57 +0500

    fix: array data structure

--- src/Array.java
+++ src/Array.java
@@ -1,11 +1,21 @@
+
 import java.util.Arrays;
 
+@SuppressWarnings("unchecked")
 public class Array<T> {
     private T[] items;
     private int count;
 
     public Array(int length) {
         items = (T[]) new Object[length];
+    }
+
+    public T[] getItems() {
+        T[] newItems = (T[]) new Object[count];
+        for (int i = 0; i < count; i++) {
+            newItems[i] = items[i];
+        }
+        return newItems;
     }
 
     private void resizeIfRequired() {
@@ -25,7 +35,7 @@ public class Array<T> {
         items[count++] = item;
     }
 
-    public void insertAt(T item, int index) {
+    public void insertAt(int index, T item) {
         if (index < 0 || index > count)
             throw new IllegalArgumentException();
 
@@ -42,28 +52,24 @@ public class Array<T> {
         if (index < 0 || index > count)
             throw new IllegalArgumentException();
 
-        for (int i = index; i < count; i++)
-            items[i] = items[i + 1];
+        for (int i = index; i < count; i++) {
+            var nextIndex = i + 1;
+            if (items.length != nextIndex)
+                items[i] = items[nextIndex];
+        }
 
-        --count;
+        count--;
     }
 
     public int indexOf(T item) {
-        for (int i = 0; i <= count; i++)
+        for (int i = 0; i < count; i++)
             if (item == items[i])
                 return i;
 
         return -1;
     }
 
-    public void print() {
-        System.out.println(Arrays.toString(items));
-    }
-
     public T max() {
-        if (count == -1)
-            throw new IllegalAccessError();
-
         if (count == 0)
             return items[0];
 
@@ -78,22 +84,22 @@ public class Array<T> {
     }
 
     public T[] reverse() {
-        T[] newArray = (T[]) new Object[items.length];
+        T[] newItems = (T[]) new Object[count];
 
-        int activeIndex = -1;
-        for (int i = count; i >= 0; i--) {
-            newArray[++activeIndex] = items[i];
-        }
+        for (int i = 0; i < count; i++)
+            newItems[i] = items[count - i - 1];
 
-        return newArray;
+        return newItems;
     }
 
-    public Array intersect(Array other) {
-        var intersection = new Array(count);
+    public Array<T> intersect(Array<T> other) {
+        var intersection = new Array<T>(count);
 
-        for (T item : items)
-            if (other.indexOf(item) >= 0)
-                intersection.insert(item);
+        for (int i = 0; i < count; i++) {
+            if (other.indexOf(items[i]) >= 0) {
+                intersection.insert(items[i]);
+            }
+        }
 
         return intersection;
     }
@@ -101,14 +107,27 @@ public class Array<T> {
 
 class TestArray {
     public static void main(String[] args) {
-        Array<Integer> numbers = new Array<>(2);
+        Array<Integer> numbers = new Array<>(1);
 
-        numbers.insert(1);
+        numbers.insert(99);
         numbers.insert(2);
-        numbers.insert(3);
+        numbers.insert(4);
 
-        numbers.insertAt(99, 2);
+        numbers.insertAt(2, 3);
+        numbers.removeAt(0);
 
-        numbers.print();
+        System.out.println(Arrays.toString(numbers.getItems()));
+        System.out.printf("reverse %s\n", Arrays.toString(numbers.reverse()));
+        System.out.printf("index of `1` is %s.\n", Integer.toString(numbers.indexOf(1)));
+        System.out.printf("index of `2` is %s.\n", Integer.toString(numbers.indexOf(2)));
+        System.out.printf("index of `4` is %s.\n", Integer.toString(numbers.indexOf(4)));
+        System.out.printf("max %s.\n", Integer.toString(numbers.max()));
+
+        Array<Integer> moreNumbers = new Array<>(4);
+
+        moreNumbers.insert(4);
+        moreNumbers.insert(5);
+
+        System.out.println(Arrays.toString(numbers.intersect(moreNumbers).getItems()));
     }
 }
